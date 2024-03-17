394f65e867921a7e9f8ddd3d1daf072ebfc692c5
Tomas Gonzalez <csctomasgonzalez@gmail.com>
2024-03-17 17:02:32 -0600

    cambios práctica 10

--- practica 10.py
+++ practica 10.py
@@ -5,6 +5,16 @@ class Heore:
         self.nivel = nivel
         self.clase = clase
 
+
+#es un método especial que se utiliza para definir
+# cómo se debe representar una instancia de una clase
+# como una cadena de texto. Este método es llamado
+# automáticamente cuando se intenta convertir un objeto
+# en una cadena de texto utilizando la función str()
+# o cuando el objeto es utilizado en contextos donde
+# se espera una representación de cadena, como al
+# imprimirlo con la función print().
+
     def __str__(self):
         return (f"{self.nombre}" +
                 f" es un {self.clase} de nivel {self.nivel}")
